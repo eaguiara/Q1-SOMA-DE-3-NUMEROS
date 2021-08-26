e8df1b738cf9c0d65ed70e178cc0811b3e9a1616
Eduarda Aguiar <73914795+eaguiara@users.noreply.github.com>
2021-08-26 22:43:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def soma(a, b, c):
+    s = a + b + c
+    return s
+
+
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+c = int(input('Terceiro número: '))
+
+print('Soma = ', soma(a, b, c))
